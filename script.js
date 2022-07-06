f53b0de452ee2cdea98b2c819bec0ee5ff499b63
snesho <sneshogungu987@gmail.com>
2022-07-06 09:52:06 +0200

    added js and changed

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var state = {
     balance: 0,
     income: 0,
     expense: 0,
-    transaction = [
-        {name:'',amount: 0, type: income}
+    transactions: [
+        { name:'Salary',amount: 0, type: 'income' },
+        { name:'Transport',amount: 0, type: 'expense' },
+        { name:'insurance',amount: 0, type: 'expense' },
     ],
+}
+
+function init() {
+    console.log(balance);
 }
     
