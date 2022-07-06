0d7315afafa3b6a26e1a23f66467e93c139d96a8
snesho <sneshogungu987@gmail.com>
2022-07-06 15:09:01 +0200

    changed function and added

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ var state = {
         { name:'Salary',amount: 0, type: 'income' },
         { name:'Transport',amount: 0, type: 'expense' },
         { name:'insurance',amount: 0, type: 'expense' },
-    ],
+    ]
 }
 
+var balanceEl = document.querySelector('#balance');
+var incomeEl = document.querySelector('#income');
+var expenseEl = document.querySelector('#expense');
+var transactionsEl = document.querySelector('#transaction');
+
 function init() {
-    console.log('balance');
+    balanceEl.innerHTML = `R${state.balance}`;
+    incomeEl.innerHTML = `R${state.income}`;
+    expenseEl.innerHTML = `R${state.expense}`;
+
+    var transactionsEl;
+
+    for (var i = 0; i < state.transactions.length; i++) {
+        
+    }
 }
+
 
 init();
     
