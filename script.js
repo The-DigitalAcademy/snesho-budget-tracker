be8cfc05ba9cceb247fe4568c7fcbda572069960
snesho <sneshogungu987@gmail.com>
2022-07-22 16:14:46 +0200

    fixed js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var state = {
     income: 1500,
     expense: 500,
     transactions: [
-        {type: 'income', detail: 'Salary', amount: 5000},
-        {type: 'expense', detail: 'Buy', amount: 50},
-        {type: 'expense', detail: 'Transport', amount: 500} 
+        {name: 'Salary', amount: 5000, type: 'income'},
+        {name: 'Transport', amount: 500, type: 'expense'},
+        {name: 'Buy Grocery', amount: 230, type: 'expense'},
     ]
 }
 
@@ -14,13 +14,41 @@ var incomeEl = document.querySelector('#income');
 var expenseEl = document.querySelector('#expense');
 var transactionsEl = document.querySelector('#transaction');
 
-// link JAVASCRIPT to HTML
-function display() {
+function init() {
+    updateState();
+    render();
+}
+
+function updateState() {
+    var balance = 0,
+        income = 0,
+        expense = 0;
+        item;
+    
+        for (var i = 0; i < state.transactions.length; i++) {
+            item = state.transactions[i];
+
+            if (item.type === 'income') {
+                income += item.amount;
+            } else if (item.type === 'expense') {
+                expense += item.amount;
+            }
+        }
+
+        balance = income - expense;
+    
+        state.balance = balance;
+        state.income = income;
+        state.expense = expense;
+
+}
+
+function render() {
     balanceEl.innerHTML = `R${state.balance}`;
     incomeEl.innerHTML = `R${state.income}`;
     expenseEl.innerHTML = `R${state.expense}`;
 
-    var transactionEl, containerEl, amountEl, item;
+    var transactionEl, containerEl, amountEl, item, btnEl;
 
     for (var i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
@@ -45,4 +73,4 @@ function display() {
     transactionsEl.appendChild()
 }
 
-display();
+init();
