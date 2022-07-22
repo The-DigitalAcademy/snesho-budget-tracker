fe7b10c1a570107a54fcacea83b2d47f90f0a204
snesho <sneshogungu987@gmail.com>
2022-07-22 14:54:24 +0200

    changed btn

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var state = {
-    balance: 500,
-    income: 0,
-    expense: 0,
+    balance: 200,
+    income: 1500,
+    expense: 500,
     transactions: [
         {type: 'income', detail: 'Salary', amount: 5000},
         {type: 'expense', detail: 'Buy', amount: 50},
-        {type: 'expense', detail: 'Transport', amount: 500},
-    ],
+        {type: 'expense', detail: 'Transport', amount: 500} 
+    ]
 }
 
-var balanceEl = document.querySelector('balance');
-var incomeEl = document.querySelector('income');
-var expenseEl = document.querySelector('expense');
-var transactionsEl = document.querySelector('transaction');
+var balanceEl = document.querySelector('#balance');
+var incomeEl = document.querySelector('#income');
+var expenseEl = document.querySelector('#expense');
+var transactionsEl = document.querySelector('#transaction');
 
 // link JAVASCRIPT to HTML
 function display() {
@@ -20,18 +20,29 @@ function display() {
     incomeEl.innerHTML = `R${state.income}`;
     expenseEl.innerHTML = `R${state.expense}`;
 
-    var transaction = document.getElementById(transaction);
+    var transactionEl, containerEl, amountEl, item;
 
     for (var i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
-        transactionsEl = document
-    }
+        transactionEl = document.createElement('li');
+        transactionEl.append(item.name);
 
+        transactionsEl.appendChild(transactionEl);
+
+        containerEl = document.createElement('div');
+        amountEl = document.createElement('span');
+        if (item.type === 'income') {
+            amountEl.classlist.add('income-amt');
+        } else if (item.type === 'expense') {
+            amountEl.classlist.add('expense-amt');
+        }
+           
+    }
+    amountEl.innerHTML = `R${item.amount}`;
+    
+    containerEl.appendChild(amountEl);
+
+    transactionsEl.appendChild()
 }
 
-
-// // display everything
-// function display() {
-//     console.log('display everthing')
-
-display()
+display();
