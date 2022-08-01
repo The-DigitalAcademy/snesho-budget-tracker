796df29468094f0dc8a5679409529a25531d0605
snesho <sneshogungu987@gmail.com>
2022-08-01 09:11:14 +0200

    fixed everthing

--- script.js
+++ script.js
@@ -1,3 +1,163 @@
+// var state = {
+//     balance: 1000,
+//     income: 400,
+//     expense: 100,
+//     transactions: [
+        
+//     ]
+// }
+
+// var balanceEl = document.querySelector('#balance');
+// var incomeEl = document.querySelector('#income');
+// var expenseEl = document.querySelector('#expense');
+// var transactionsEl = document.querySelector('#transaction');
+// var incomeBtnEl = document.querySelector('#incomeBtn');
+// var expenseBtnEl = document.querySelector('#expenseBtn');
+// var nameInputEl = document.querySelector('#name');
+// var amountEl = document.querySelector('#amount')
+
+// function init() {
+//     var localState = JSON.parse(localStorage.getItem('budget tracker'));
+
+//     if (localState !== null) {
+//         state = localState;
+//     }
+
+//     updateState();
+//     initListeners();
+// }
+
+// function uniqueId() {
+//     return Math.random(Math.random() * 1000000);
+// }
+
+// function initListeners() {
+//     incomeBtnEl.addEventListener('click', onAddIncomeClick);
+//     expenseBtnEl.addEventListener('click', onAddExpenseClick);
+// }
+
+// // DRY - Do not repeat yourself
+
+// function onAddIncomeClick() {
+//     addTransaction(nameInputEl.value, amountInputEl.value, 'income');    
+
+
+// }
+
+// function addTransaction(name, amount, type) {
+
+
+//     if (name !== '' && amount !== '') {
+//         var transaction = {
+//             id: uniqueId(),
+//             name: name,
+//             amount: parseInt(amount),
+//             type: type
+//         };
+
+//         state.transactions.push(transaction);
+
+//         updateState();
+//     } else {
+//         alert('Please enter valid data');
+//     }
+
+//     nameInputEl.value = '';
+//     amountInputEl.value = '';   
+
+   
+// }
+
+// function onAddExpenseClick() {
+//     addTransaction(nameInputEl.value, amountInputEl.value, 'expense');
+// } 
+
+// function onDeleteClick(event) {
+//     var id = parseInt(event.target.getAttribute('data-id'));
+//     var deleteIndex;
+//     for (var i = 0; i < state.transactions.length; i++) {
+//         if (state.transactions[i].id === id) {
+//             deleteIndex = i;
+//             break;
+//         }
+//     }
+
+//     state.transactions.splice(deleteIndex, 1);
+
+//     updateState();
+// }
+ 
+// function updateState() {
+//     var balance = 0,
+//         income = 0,
+//         expense = 0,
+//         item;
+
+//     for (var i = 0; i < state.transactions.length; i++) {
+//         item = state.transactions[i];
+
+//         if (item.type === 'income') {
+//             income += item.amount;
+//         } else if (item.type === 'expense') {
+//             expense += item.amount;
+//         }
+//     }
+
+//     balance = income - expense;
+
+//     state.balance = balance;
+//     state.income = income;
+//     state.expense = expense;
+
+//     localStorage.setItem('budget tracker', JSON.stringify(state))
+
+//     render();
+// }
+
+// function render() {
+//     balanceEl.innerHTML = `R${state.balance}`;
+//     incomeEl.innerHTML = `R${state.income}`;
+//     expenseEl.innerHTML = `R${state.expense}`;
+
+//     var transactionEl, containerEl, amountEl, item, btnEl;
+
+//     transactionsEl.innerHTML = '';
+
+//     for (var i = 0; i < state.transactions.length; i++) {
+//         item = state.transactions[i];
+//         transactionEl = document.createElement('li');
+//         transactionEl.append(item.name);
+
+//         transactionsEl.appendChild(transactionEl);
+
+//         containerEl = document.createElement('div');
+//         amountEl = document.createElement('span');
+//         if (item.type === 'income') {
+//             amountEl.classList.add('income-amt');
+//         } else if (item.type === 'expense') {
+//             amountEl.classList.add('expense-amt');
+//         }
+//         amountEl.innerHTML = `R${item.amount}`;
+
+//         containerEl.appendChild(amountEl);
+
+//         btnEl = document.createElement('button');
+//         btnEl.setAttribute('data-id', item.id);
+//         btnEl.innerHTML = 'X';
+
+//         btnEl.addEventListener('Click', onDeleteClick);
+
+//         containerEl.appendChild(btnEl);
+
+//         transactionEl.appendChild(containerEl);
+//     }
+// }
+
+// init();
+
+
+
+
 var state = {
     balance: 1000,
     income: 400,
@@ -14,10 +174,10 @@ var transactionsEl = document.querySelector('#transaction');
 var incomeBtnEl = document.querySelector('#incomeBtn');
 var expenseBtnEl = document.querySelector('#expenseBtn');
 var nameInputEl = document.querySelector('#name');
-var amountEl = document.querySelector('#amount')
+var amountInputEl = document.querySelector('#amount');
 
 function init() {
-    var localState = JSON.parse(localStorage.getItem('budget tracker'));
+    var localState = JSON.parse(localStorage.getItem('expenseTrackerState'));
 
     if (localState !== null) {
         state = localState;
@@ -28,7 +188,7 @@ function init() {
 }
 
 function uniqueId() {
-    return Math.random(Math.random() * 1000000);
+    return Math.round(Math.random() * 1000000);
 }
 
 function initListeners() {
@@ -39,10 +199,15 @@ function initListeners() {
 // DRY - Do not repeat yourself
 
 function onAddIncomeClick() {
-    addTransaction(nameInputEl.value, amountInputEl.value, 'income');    
+    addTransaction(nameInputEl.value, amountInputEl.value, 'income');
 }
 
+
+
 function addTransaction(name, amount, type) {
+    var name = nameInputEl.value;
+    var amount = amountInputEl.value;
+
     if (name !== '' && amount !== '') {
         var transaction = {
             id: uniqueId(),
@@ -54,58 +219,63 @@ function addTransaction(name, amount, type) {
         state.transactions.push(transaction);
 
         updateState();
+
     } else {
-        alert('Please enter valid data');
+        alert("Please enter valid data");
     }
 
     nameInputEl.value = '';
-    amountInputEl.value = '';   
+    amountInputEl.value = '';
 }
 
 function onAddExpenseClick() {
     addTransaction(nameInputEl.value, amountInputEl.value, 'expense');
-} 
+}
 
 function onDeleteClick(event) {
     var id = parseInt(event.target.getAttribute('data-id'));
     var deleteIndex;
-    for (var i = 0; i < state.transactions.length; i++) {
+
+    for (let i = 0; i < state.transactions.length; i++) {
         if (state.transactions[i].id === id) {
             deleteIndex = i;
             break;
         }
     }
-
     state.transactions.splice(deleteIndex, 1);
 
     updateState();
+
 }
- 
-function updateState() {
+
+function updateState(){
     var balance = 0,
-        income = 0,
-        expense = 0,
-        item;
+    income = 0,
+    expense = 0,
+    item;
 
     for (var i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
 
         if (item.type === 'income') {
             income += item.amount;
-        } else if (item.type === 'expense') {
+        }
+        else if (item.type === 'expense') {
             expense += item.amount;
         }
     }
 
     balance = income - expense;
 
+   
     state.balance = balance;
     state.income = income;
     state.expense = expense;
 
-    localStorage.setItem('budget tracker', JSON.stringify(state))
+    localStorage.setItem('expenseTrackerState', JSON.stringify(state));
 
     render();
+
 }
 
 function render() {
@@ -115,9 +285,9 @@ function render() {
 
     var transactionEl, containerEl, amountEl, item, btnEl;
 
-    transactionEl.innerHTML = '';
+    transactionsEl.innerHTML = '';
 
-    for (var i = 0; i < state.transactions.length; i++) {
+    for (let i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
         transactionEl = document.createElement('li');
         transactionEl.append(item.name);
@@ -139,12 +309,14 @@ function render() {
         btnEl.setAttribute('data-id', item.id);
         btnEl.innerHTML = 'X';
 
-        btnEl.addEventListener('Click', onDeleteClick);
+        btnEl.addEventListener('click', onDeleteClick);
 
         containerEl.appendChild(btnEl);
 
         transactionEl.appendChild(containerEl);
+
     }
 }
+
 
 init();
